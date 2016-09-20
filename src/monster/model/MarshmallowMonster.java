602f3ec37cd9ce28656ad65653b2137a9ddb2060
mradams276 <kyle.adams276@gmail.com>
2016-09-20 09:11:27 -0600

    Added setters and getters

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -33,17 +33,54 @@ public class MarshmallowMonster
 		this.legCount = legCount;
 		this.antennaCount = antennaCount;
 		this.hairCount = hairCount;
-		this.hasBellyButton = hasBellyButton;
-		
+		this.hasBellyButton = hasBellyButton;	
 	}
 
+		public String toString()
+		{
+			String description = "Name is" + this.name;
+			
+			return description;
+		}
 
+		public String getName()
+		{
+			return name;
+		}
 
+		public int getEyeCount()
+		{
+			return eyeCount;
+		}
 
+		public void setName(String name)
+		{
+			this.name = name;
+		}
 
+		public void setEyeCount(int eyeCount)
+		{
+			this.eyeCount = eyeCount;
+		}
 
+		public void setLegCount(int legCount)
+		{
+			this.legCount = legCount;
+		}
 
+		public void setAntennaCount(int antennaCount)
+		{
+			this.antennaCount = antennaCount;
+		}
 
+		public void setHairCount(double hairCount)
+		{
+			this.hairCount = hairCount;
+		}
 
+		public void setHasBellyButton(boolean hasBellyButton)
+		{
+			this.hasBellyButton = hasBellyButton;
+		}
 
 }
